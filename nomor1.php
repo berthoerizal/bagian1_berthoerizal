f8befd35c9497508b4331f0fc4ef4508ff1e5279
Bertho Erizal <berthoerizal21@gmail.com>
2021-10-01 22:09:40 +0700

    bagian 1 nomor 1

--- nomor1.php
+++ nomor1.php
@@ -1,13 +1,21 @@
-<?php
-$nilai = array(72,65,73,78,75,74,90,81,87,65,55,69,72,78,79,91,100,40,67,77,86);  // Average 
-$jumlah = array_sum($nilai); 
-$rata = $jumlah / count ( $nilai ); 
-// Top 7 Higher 
-rsort($nilai); 
-$top7 = array_slice($nilai, 0, 7 );
-echo "Tertinggi : "; print_r($top7); 
-krsort($nilai);
-$low7 = array_slice($nilai, 0, 7);
-echo "Terendah : "; print_r($low7); 
-echo " ";
-echo "Rata-Rata : ".$rata; echo " ";
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Nomor 1</title>
+</head>
+
+<body>
+    <?php
+    $nilai = "72 65 73 78 75 74 90 81 87 65 55 69 72 78 79 91 100 40 67 77 86";
+    $nilai_array = explode(" ", $nilai);
+    echo '(1) Nilai Rata-rata = ' . array_sum($nilai_array) / count($nilai_array) . "<br />";
+    echo '(2) Nilai Tertiinggi = ' . max($nilai_array) . "<br />";
+    echo '(3) Nilai Terendah = ' . min($nilai_array);
+    ?>
+</body>
+
+</html>
